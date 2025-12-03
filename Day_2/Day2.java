61f8575b3600d7a794877400650849a812c3b6e5
qnert <qnertsimon@gmail.com>
2025-12-03 19:09:40 +0100

    added day2 the second challenge it got accepted and works fine, not the fastest solution but the output is key not the runtime

--- Day_2/Day2.java
+++ Day_2/Day2.java
@@ -4,30 +4,41 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Day2{
-    public static boolean containsRepeatedSubstring(String input){
-            Pattern pattern = Pattern.compile("^(\\d+)\\1$");
+    static long counter_first = 0;
+    static long counter_second = 0;
+
+    public static boolean containsTwiceRepeatedSubstring(String input){
+            Pattern pattern = Pattern.compile("^([0-9]+)\\1$");
             Matcher matcher = pattern.matcher(input);
             
             return matcher.find();
     }
 
-	private static long findInvalidID(String line){
-        long counter = 0;
+    public static boolean containsAnyRepeatedSubstring(String input){
+            Pattern pattern = Pattern.compile("^([0-9]+)\\1+$");
+            Matcher matcher = pattern.matcher(input);
+            
+            return matcher.find();
+    }
+
+	private static void findInvalidID(String line){
         String[] range = line.split("-");
         long end = Long.parseLong(range[1]);
         long start = Long.parseLong(range[0]);
 
         for (long i = start; i <= end; i++){
-            if (containsRepeatedSubstring(i + "")){
-                counter += i;
+            if (containsTwiceRepeatedSubstring(i + "")){
+                counter_first += i;
+            }
+            if (containsAnyRepeatedSubstring(i + "")){
+                counter_second += i;
             }
         }
-        return counter;
+        return;
 	}
 
-	private static long getInputAndOperate(String file){
+	private static void getInputAndOperate(String file){
         String[] line;
-        long counter = 0;
 		Scanner scan = null;
 		try {
     		scan = new Scanner(new File(file));
@@ -36,14 +47,16 @@ public class Day2{
 		}
         line = scan.next().split(",");
         for (String word: line){
-            counter += findInvalidID(word);
+            findInvalidID(word);
         }
 		scan.close();
-		return counter;
+        System.out.println(counter_first);
+        System.out.println(counter_second);
+		return ;
 	}
 
 	public static void main(String[] argv){
-		System.out.println(getInputAndOperate(argv[0]));
+		getInputAndOperate(argv[0]);
 		return;
 	}
 }
