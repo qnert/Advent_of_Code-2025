da266fbc6ab5ed217f8456745dd31d0498fdfbc9
qnert <qnertsimon@gmail.com>
2025-12-03 19:00:50 +0100

    added the second solution and tried to do everything in one file while taking the input from the argvs

--- Day_1/Day1.java
+++ Day_1/Day1.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Day1{
-	private static int operate(String line, int nb){
+	static int value_first = 50;
+	static int value_second = 50;
+	static int counter_first = 0;
+	static int counter_second = 0;
+
+	private static int operate_first(String line, int nb){
 		int curr = Integer.parseInt(line.substring(1));
+		
 		if (line.charAt(0) == 'L'){
 				nb -= curr;
 				if (nb < 0){nb = 100 + nb;}
@@ -11,31 +17,53 @@ public class Day1{
 		else if (line.charAt(0) == 'R'){
 				nb += curr % 100;
 		}
+		if ((nb % 100) == 0)
+			counter_first++;
 		return nb % 100;
 	}
 
-	private static int getInputAndOperate(String file){
-		int counter = 0;
-		int value = 50;
-		Scanner scan = null;
+	private static int operate_second(String line, int nb){
+        char dir = line.charAt(0);
+        int move = Integer.parseInt(line.substring(1));
+
+        for (int i = 0; i < move; i++) {
+            if (dir == 'L') {
+                nb--;
+                if (nb < 0) 
+					nb = 99;
+            } else if (dir == 'R') {
+                nb++;
+                nb %= 100;
+            }
+            if (nb == 0) 
+				counter_second++;
+        }
+        return nb;
+    }
+
+	private static void getInputAndOperate(String file){
 		String lines = "";
+		Scanner scan = null;
+		String curr_str = "";
+
 		try {
     		scan = new Scanner(new File(file));
 		}catch (FileNotFoundException e) {
     		e.printStackTrace();
 		}
 		while (scan.hasNext()) {
-			value = operate(scan.next(), value);
-			if (value == 0){
-					counter++;
-			}
+			curr_str = scan.next();
+			value_first = operate_first(curr_str, value_first);
+			value_second = operate_second(curr_str, value_second);
 		}
 		scan.close();
-		return counter;
+		System.out.println(counter_first);
+		System.out.println(counter_second);
+		return;
 	}
 
 	public static void main(String[] argv){
-		System.out.println(getInputAndOperate(argv[0]));
+		getInputAndOperate(argv[0]);
 		return;
 	}
 }
