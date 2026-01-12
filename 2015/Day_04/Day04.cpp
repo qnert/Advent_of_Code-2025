94249654f645fb221e2d41f0dc4c06e2d444d076
qnert <qnertsimon@gmail.com>
2026-01-12 20:01:18 +0100

    added the day04 working part one and part two

--- 2015/Day_04/Day04.cpp
+++ 2015/Day_04/Day04.cpp
@@ -7,13 +7,36 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
-#include "md5.h"
+#include <openssl/md5.h>
+
+std::string md5(const std::string& input) {
+    unsigned char digest[MD5_DIGEST_LENGTH];
+    MD5((unsigned char*)input.c_str(), input.size(), digest);
+
+    std::ostringstream oss;
+    for (int i = 0; i < MD5_DIGEST_LENGTH; ++i) {
+        oss << std::hex << std::setw(2) << std::setfill('0')
+            << (int)digest[i];
+    }
+    return oss.str();
+}
 
 void doOperationFirst(std::string line){
-    std::cout << line << std::endl;
+    int counter = 0;
+
+    while(!md5(line + std::to_string(counter)).starts_with("00000")){
+        counter++;
+    }
+    std::cout << md5(line + std::to_string(counter)) << " " << "Solution: " << counter << std::endl;
 }
 
 void doOperationSecond(std::string line){
+    int counter = 0;
+
+    while(!md5(line + std::to_string(counter)).starts_with("000000")){
+        counter++;
+    }
+    std::cout << md5(line + std::to_string(counter)) << " " << "Solution: " << counter << std::endl;
 }
 
 int main(int ac, char** av){
@@ -27,6 +50,7 @@ int main(int ac, char** av){
         inFile.close();
     }
     doOperationFirst(line);
+    doOperationSecond(line);
     return 0;
 }
 
